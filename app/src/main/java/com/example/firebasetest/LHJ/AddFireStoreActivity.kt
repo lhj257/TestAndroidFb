8560762cc3959cd5519f031d9083c3f36f19e736
lhj257 <139347455+lhj257@users.noreply.github.com>
2023-11-20 16:35:20 +0900

    231120 7교시 파이어 스토어 글업데이트 테스트

--- app/src/main/java/com/example/firebasetest/LHJ/AddFireStoreActivity.kt
+++ app/src/main/java/com/example/firebasetest/LHJ/AddFireStoreActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.firebasetest.LHJ.MyApplication.Companion.db
 import com.example.firebasetest.LHJ.Utils.MyUtil
 import com.example.firebasetest.LHJ.databinding.ActivityAddFireStoreBinding
 import java.util.Date
@@ -31,6 +32,7 @@ class AddFireStoreActivity : AppCompatActivity() {
                 .addOnCompleteListener {
                     Log.d("lhj","글쓰기 성공")
                     Toast.makeText(this,"글쓰기 성공",Toast.LENGTH_SHORT).show()
+                    binding.inputEdt.text.clear()
                 }
                 //데이터 추가 실패후 실행할 콜백함수
                 .addOnFailureListener {
@@ -38,5 +40,57 @@ class AddFireStoreActivity : AppCompatActivity() {
                     Toast.makeText(this,"글쓰기 실패",Toast.LENGTH_SHORT).show()
                 }
         }
+
+        //불러오기 ,리사이클러뷰 이용해서, 조금있다가하고
+        // 현재 한개만 샘플로 불러오기.
+        // 공식문서 :
+        // https://firebase.google.com/docs/firestore/query-data/get-data?hl=ko#kotlin+ktx
+            val docRef = db.collection("TestBoard").document("ufEBY5MyyOLxObovU3Uc")
+            docRef.get()
+                .addOnSuccessListener { document ->
+                    if (document != null) {
+                        Log.d("lhj", "DocumentSnapshot data: ${document.data}")
+                        Toast.makeText(this,"DocumentSnapshot data: ${document.data}",Toast.LENGTH_SHORT).show()
+                        binding.resultStoreView.text = document.data.toString()
+                    } else {
+                        Log.d("lhj", "No such document")
+                        Toast.makeText(this,"No such document",Toast.LENGTH_SHORT).show()
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.d("lhj", "get failed with ", exception)
+                }
+
+        //글 하나 삭제하기.
+        //공식 문서
+        //https://firebase.google.com/docs/firestore/manage-data/delete-data?hl=ko
+        binding.deleteBtn.setOnClickListener {
+            db.collection("TestBoard").document("ufEBY5MyyOLxObovU3Uc")
+                .delete()
+                .addOnSuccessListener {
+                    Log.d("lhj", "DocumentSnapshot successfully deleted!")
+                    Toast.makeText(this,"삭제 성공",Toast.LENGTH_SHORT).show()}
+                .addOnFailureListener { e ->
+                    Log.w("lhj", "Error deleting document", e)
+                    Toast.makeText(this,"삭제 실패",Toast.LENGTH_SHORT).show()}
+        }
+
+        //하나 업데이트
+        //공식문서
+        //https://firebase.google.com/docs/firestore/manage-data/add-data?hl=ko
+        binding.updateBtn.setOnClickListener {
+        val washingtonRef = db.collection("TestBoard").document("UVJti7uJFNdAtzxm0753")
+
+// Set the "isCapital" field of the city 'DC'
+        washingtonRef
+            .update("capital", true)
+            .addOnSuccessListener {
+                Log.d("lhj", "DocumentSnapshot successfully updated!")
+                Toast.makeText(this,"수정 성공",Toast.LENGTH_SHORT).show()}
+            .addOnFailureListener { e ->
+                Log.w("lhj", "Error updating document", e)
+                Toast.makeText(this,"수정 실패",Toast.LENGTH_SHORT).show()}
+        }
     }
+
 }
