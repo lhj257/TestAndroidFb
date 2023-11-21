26aa3cb50c9d7e2d8c72261ef8b6838d7614f3c7
lhj257 <139347455+lhj257@users.noreply.github.com>
2023-11-21 11:48:54 +0900

    231121 3교시

--- app/src/main/java/com/example/firebasetest/LHJ/imageShareApp/MainImageShareAppActivity.kt
+++ app/src/main/java/com/example/firebasetest/LHJ/imageShareApp/MainImageShareAppActivity.kt
@@ -1,19 +1,63 @@
 package com.example.firebasetest.LHJ.imageShareApp
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.firebasetest.LHJ.AuthActivity
+import com.example.firebasetest.LHJ.MyApplication
 import com.example.firebasetest.LHJ.R
+import com.example.firebasetest.LHJ.Utils.MyUtil
 import com.example.firebasetest.LHJ.databinding.ActivityMainImageShareAppBinding
 
 //스토어, 스토리지에서 데이터를 받아서, 리사이클러뷰로 출력할 예정.
 // 인증, 구글인증, 이메일, 패스워드 인증 재사용.
-// 인증이 되어야 글쓱 ㅣ가능하게 하고,
+// 인증이 되어야 글쓰기 가능하게 하고,
 // 일단, 삭제 한번 도전해보고,
 // 수정 도전해보기.
 class MainImageShareAppActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainImageShareAppBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_image_share_app)
+        binding = ActivityMainImageShareAppBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //툴바 붙이기
+        setSupportActionBar(binding.toolbarMain)
+
+        //해당 메인 앱이 실행이 되면, 먼저, 현재 앱의 외부 스토리지에 접근 권한 체크하기.
+        MyUtil.checkPermission(this)
+
+        //플로팅 액션 버튼 클릭시, 글쓰기 뷰로 이동하기 작업.
+        binding.addFabtn.setOnClickListener{
+            //글쓰기 전에, 인증 여부 확인 하기
+            if(MyApplication.checkAuth()){
+                //글쓰기 페이지 이동 -> AddImageShareApp
+                //이름 수정 : AddImageShareAppActivity
+                startActivity(Intent(this,AddImageShareAppActivity::class.java))
+            }else{
+                Toast.makeText(this,"인증후 글쓰기 해주세요",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }//onCreate
+
+    //메뉴 붙이기. 작업.
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.auth_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    //붙인 메뉴 이벤트 처리하기.
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId === R.id.menu_add_auth){
+           //인증 후 실행할로직
+            //AuthActivity 구성해서, 이 화면으로 이동하게끔 설정.
+            startActivity(Intent(this,AuthActivity::class.java))
+        }
+        // 저장 구성, 인증은 메인으로 옮기기
+        return super.onOptionsItemSelected(item)
     }
 }
